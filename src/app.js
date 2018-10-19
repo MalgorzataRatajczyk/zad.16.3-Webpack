33f08d1225738938e0e1f1b108498882df08add0
Malgorzata Ratajczyk <malgorzataratajczyk@wp.pl>
2018-10-19 15:15:42 +0200

    Init and configuration project

---
+++ src/app.js
@@ -0,0 +1,4 @@
+"use strict"
+
+import world from './world';
+console.log('hello ' + world);
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+"use strict"
+
+const path = require('path');
+
+//webpack.config.js
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    }
+};
